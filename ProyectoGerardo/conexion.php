3c0fe2801460d95c104ef6834e93cecc34b930ce
Roque Davila Juan Carlos <113048169+RoqueDavilaJuanCarlos@users.noreply.github.com>
2023-03-11 16:35:37 -0600

    Add files via upload

---
+++ ProyectoGerardo/conexion.php
@@ -0,0 +1,30 @@
+<?php
+// Conectarse a la base de datos
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$dbname = "registro";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Verificar la conexión
+if (!$conn) {
+    die("Conexión fallida: " . mysqli_connect_error());
+}
+echo "Conexión exitosa";
+
+$nombre = $_POST["nombre"] ;
+$apellido = $_POST["apellido"] ;
+$usuario = $_POST["usuario"] ;
+$contraseña = $_POST["contraseña"] ;
+
+// Agregar datos a la tabla
+$sql = "INSERT INTO users (nombre, apellido, usuario, contraseña)
+VALUES ('$nombre', '$apellido', '$usuario', '$contraseña')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Datos agregados correctamente";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+?>
