6b64d84a81e110a979c34ac77210e8f8871bd86d
Roque Davila Juan Carlos <113048169+RoqueDavilaJuanCarlos@users.noreply.github.com>
2023-03-13 20:02:49 -0600

    Add files via upload
    ultima modificacion del proyecto

--- ProyectoGerardo/conexion.php
+++ ProyectoGerardo/conexion.php
@@ -1,17 +1,23 @@
 <?php
 // Conectarse a la base de datos
-$servername = "localhost:3307";
+// El servername es el  nombre del servidor que estamos usando
+$servername = "localhost:3306";
+// Username es el nombre del usuario
 $username = "root";
+// El password es la contraseña a nuestro servidor la cual nosotros no tenemos
 $password = "";
+// El dbname es el nombre de nuestra base de datos 
 $dbname = "registro";
 
+// Aqui hacemos la coneccion con mysql , damos validados los datos anteriormente
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-// Verificar la conexión
+// Verifica la conexión
 if (!$conn) {
     die("Conexión fallida: " . mysqli_connect_error());
 }
 echo "Conexión exitosa";
 
+// Aqui validamos nuestras variables que se registraron en el codigo de html
 $nombre = $_POST["nombre"] ;
 $apellido = $_POST["apellido"] ;
 $usuario = $_POST["usuario"] ;
@@ -20,7 +26,8 @@ $contraseña = $_POST["contraseña"] ;
 // Agregar datos a la tabla
 $sql = "INSERT INTO users (nombre, apellido, usuario, contraseña)
 VALUES ('$nombre', '$apellido', '$usuario', '$contraseña')";
-
+// Aqui nos da el comentario de datos agrgados correctamente para decirnos
+// que los datos fueron agregados correctamente
 if (mysqli_query($conn, $sql)) {
     echo "Datos agregados correctamente";
 } else {
